2679555e74cb3e904e57f1712ca27b52f98d1563
Erin <erinylee@yahoo.com>
2016-08-19 14:52:00 -0700

    testing

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a,b){
   var myFirst = //eslint-disable-line
-
+***
 }
 
 // Here is the test for multiply(); uncomment it to run it
