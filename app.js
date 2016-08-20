f4d0f6c74dd729a217a414e938fbd3c807035a78
Erin <erinylee@yahoo.com>
2016-08-20 00:43:52 -0700

    changed the return on #2

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ Test this function by hand in the console to get it working, and when you think 
 function testMultiply(a,b){
   var firstMul = a * b ;
   var secMsg = 'The product of 5 and 9 is ';
-  return(secMsg, firstMul) //eslint-disable-line
+  return(secMsg + firstMul) //eslint-disable-line
 };
 
 testMultiply(5,9);
@@ -53,12 +53,13 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a,b,c){ //eslint-disable-line
+// function sumAndMultiply(a,b,c){
+//   var sum1 = a + b + c;
+//   var mult = a * b * c;
+//   return('4 and 7 and 5 sum to ' + sum1 + ' . The product of 4 ad 7 and 5 is ' + mult )//eslint-disable-line
+// }
 
-}
-
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
