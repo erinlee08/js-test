193d23c0cd64aa4575b872a5dff280a6c7c6603e
Erin <erinylee@yahoo.com>
2016-08-20 19:14:13 -0700

    question4

--- app.js
+++ app.js
@@ -73,20 +73,33 @@ Write a function called sumArray() that takes in an array of numbers as its sing
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+IMPORTANT DETAIL: You may not use the arithmetic ope rator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-var testArray = [2,3,4]; //eslint-disable-line
 
+/* Problem 4
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+"2,3,4 was passed in as an array of numbers, and 9 is their sum."
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+
+// Write your code here
+
+var testArray = [2,3,4];
+var sum = '4 and 7 and 5 sum to 16.'
 function sumArray(testArray){
-  if (mySum === 5) {
-    return('2,3,4 was passed in as an array of numbers, and 9 is their sum');
-  };
-testArray[];
-
-// Here is the test for sumArray(); uncomment it to run it
+  var myArr = '2,3,4 was passed in as an array of numbers.';
+  var myStr = '9 is their sum';
+  if (myArr !== sum && !(myStr === sum)) {
+    return('2,3,4 was passed in as an array of numbers, and 9 is their sum. ' + 'green');
+  } else {
+    return('red');
+  }
+}
+sumArray(testArray);
+"2,3,4 was passed in as an array of numbers, and 9 is their sum. green"// Here is the test for sumArray(); uncomment it to run it
 
 // testSumArray(testArray);
 
